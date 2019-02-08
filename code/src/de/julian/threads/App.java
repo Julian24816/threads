44749c4ec800ec50d39e4eedbe7395a6a0d59b88
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2019-02-08 07:42:21 +0100

    split main method into App object and pattern/threads setup

--- code/src/de/julian/threads/App.java
+++ code/src/de/julian/threads/App.java
@@ -4,21 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class App {
-    public static void main(String[] args) {
+    private App(Visualizer visualizer) {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.getContentPane().add(new BraceletVisualizerComponent(visualizer));
+        frame.pack();
+        frame.setVisible(true);
+    }
 
+    public static void main(String[] args) {
         final Pattern pattern = PatternFactory.forString(">\\\\\\///\n\\\\\\///<");
         final ColoredThread[] threads = ColoredThread.forColors(
                 Color.YELLOW, Color.WHITE, Color.GREEN, Color.BLACK,
                 Color.WHITE, Color.WHITE, Color.GREEN, Color.YELLOW);
 
-        final BasicBracelet bracelet = new BasicBracelet(pattern, threads);
-        final BasicVisualizer visualizer = new BasicVisualizer(bracelet);
-        final BraceletVisualizerComponent braceletVisualizerComponent = new BraceletVisualizerComponent(visualizer);
-        frame.getContentPane().add(braceletVisualizerComponent);
-
-        frame.pack();
-        frame.setVisible(true);
+        new App(new BasicVisualizer(new BasicBracelet(pattern, threads)));
     }
 }
